b6a15ce97c3e7c77c95d0588a2ac87d3039cc3a0
Oscar M Alvarez G <104049116+miusarname@users.noreply.github.com>
2023-02-13 11:38:14 -0500

    Tipos de variables

---
+++ 1.kt
@@ -0,0 +1,53 @@
+fun main() {
+       
+    // Variables numericas
+    
+    //Int
+    
+    var edad:Int=27 // Con tipado fuerte
+    var age =27
+    
+    //Float
+    
+   	var edad2:Float=12.3f 
+    //Si la declaro con tipado fuerte devo seguir las reglas...
+    var edad3=12.3f//<-- Si desee ponerla en float debo poner la f al final del numero de lo contrario lo tomara como Double
+    
+    //Double 12 decim
+    
+    var edad4:Double=21.5645335
+   
+    //Imprimir
+    
+    println(age - edad + edad3)
+    
+    
+    //Variable de "letras"
+    
+    
+    //Char
+    
+    var letra:Char='n'
+    
+    //String
+    
+    
+    var xd="hola mundo"
+    
+    //Punto de impresion
+    
+    //print(xd)
+    
+    //Boolean
+    
+    
+    var feliz=false
+    
+    //print(feliz)
+    
+    val escomo = "const"
+    
+    //Punto de impresion
+    
+    println(escomo)
+}
